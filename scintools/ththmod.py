d978847f8d210dff3b715150ba317395342a7e12
Daniel <danieltb@telus.net>
2020-04-07 15:02:22 -0400

    Development (#5)
    * Eigenvalue Curvature Search
    
    * Fixed Scaling
    
    * Eigenvalue Search
    
    * More Plots
    
    * Improved Astropy Compatibilty
    
    * More Comments
    
    * More Comments
    
    * modler outputs w and V
    
    * Changes to inverse mapping
    
    * Changed revmap
    
    * revmap options
    
    * Goof fixed
    
    * Added MPI tools
    
    * Fixed function calls in new functions
    
    * Removed Try
    
    * Import curve_fit
    
    * Fixed plotting in single_search
    
    * PlotFunc Tweaks
    
    * There is no try
    
    * Single Chunk Plotting Works
    
    * MPI chunk recovery function
    
    * Niagra tests
    
    * Tested MPI
    
    * Updated Plotting
    
    Co-authored-by: Daniel Baker <dbaker@cita.utoronto.ca>
    Co-authored-by: Daniel Baker <bakerd11@nia-login06.scinet.local>
    Co-authored-by: Daniel Baker <bakerd11@nia-login03.scinet.local>
    Co-authored-by: Daniel Baker <bakerd11@nia-login01.scinet.local>

--- scintools/ththmod.py
+++ scintools/ththmod.py
@@ -11,6 +11,7 @@ import astropy.units as u
 from scipy.sparse.linalg import eigsh
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
+from scipy.optimize import curve_fit
 
 def svd_model(arr, nmodes=1):
     u, s, w = np.linalg.svd(arr)
@@ -107,7 +108,7 @@ def thth_redmap(SS, tau, fd, eta, edges):
     return thth_red, edges_red
 
 
-def rev_map(thth, tau, fd, eta, edges):
+def rev_map(thth, tau, fd, eta, edges,isdspec=True):
     """
     Map back from theta-theta space to SS space
 
@@ -136,9 +137,22 @@ def rev_map(thth, tau, fd, eta, edges):
                          np.ravel(tau_map),
                          bins=(fd_edges,tau_edges),
                          weights=np.ravel(thth/np.sqrt(np.abs(2*eta*fd_map.T).value)).imag)[0]*1j
-    recov/=np.histogram2d(np.ravel(fd_map),
+    norm=np.histogram2d(np.ravel(fd_map),
                          np.ravel(tau_map),
                          bins=(fd_edges,tau_edges))[0]
+    if isdspec:
+        recov += np.histogram2d(np.ravel(-fd_map),
+                            np.ravel(-tau_map),
+                            bins=(fd_edges,tau_edges),
+                            weights=np.ravel(thth/np.sqrt(np.abs(2*eta*fd_map.T).value)).real)[0] -\
+                np.histogram2d(np.ravel(-fd_map),
+                            np.ravel(-tau_map),
+                            bins=(fd_edges,tau_edges),
+                            weights=np.ravel(thth/np.sqrt(np.abs(2*eta*fd_map.T).value)).imag)[0]*1j
+        norm+=np.histogram2d(np.ravel(-fd_map),
+                            np.ravel(-tau_map),
+                            bins=(fd_edges,tau_edges))[0] 
+    recov/=norm
     recov=np.nan_to_num(recov)
     return(recov.T)
 
@@ -217,123 +231,256 @@ def fft_axis(x, unit, pad=0):
         np.fft.fftfreq((pad+1) * x.shape[0], x[1] - x[0]).to_value(unit)) * unit
     return (fx)
 
-def sample_plot(dspec,
-                SS,
-                thth,
-                thth2,
-                SS2,
-                dspec2,
-                etas,
-                chisq,
-                time,
-                freq,
-                fd,
-                tau,
-		edges,
-                fdm,
-                taum,
-                eta_fit=0,
-                fit_str=None,
-                err_str=None):
-    if eta_fit == 0:
-        eta_fit = etas[chisq == chisq.min()].mean()
-    eta_low = etas.min()
-    eta_high = etas.max()
-    ##Plotting variables
-    SS_ext = ext_find(fd, tau)
-    dspec_ext = ext_find(time.to(u.min), freq)
-    SS_min = np.median(np.abs(SS)**2)
-    SS_max = np.max(np.abs(2 * thth2)**2) * np.exp(-3)
 
-    ##Compare model to data in plots
-    grid = plt.GridSpec(4, 2, wspace=0.4, hspace=0.3)
-    plt.figure(figsize=(8, 16))
-    plt.subplot(grid[0, 0])
+def single_search(params):
+    """
+    Curvature Search for a single chunk of a dynamic spectrum.
+    
+
+    Arguments:
+    params -- A tuple containing
+        dspec2 -- The chunk of the dynamic spectrum
+        freq2 -- The frequency channels of that chunk (with units)
+        time -- The time bins of that chunk (with units)
+        eta_low -- The lower limit of curvatures to search (with units)
+        eta_high -- the upper limit of curvatures to search (with units)
+        edges -- The bin edges for Theta-Theta
+        name -- A string filename used if plotting
+        plot -- A bool controlling if the result should be plotted
+    """
+    dspec2,freq2,time2,eta_low,eta_high,edges,name,plot=params
+
+    etas = np.linspace(eta_low, eta_high, 100) * u.us / u.mHz**2
+    npad=3
+
+    fd = fft_axis(time2, u.mHz, npad)
+    tau = fft_axis(freq2, u.us, npad)
+
+    dspec_pad = np.pad(dspec2,
+                   ((0, npad * dspec2.shape[0]), (0, npad * dspec2.shape[1])),
+                   mode='constant',
+                   constant_values=dspec2.mean())
+
+    SS = np.fft.fft2(dspec_pad)
+    SS = np.fft.fftshift(SS)
+    eigs = np.zeros(etas.shape)
+    for i in range(eigs.shape[0]):
+        try:
+            eigs[i] = Eval_calc(SS, tau, fd, etas[i], edges)
+        except:
+            eigs[i]=np.nan
+    try:
+        etas=etas[np.isfinite(eigs)]
+        eigs=eigs[np.isfinite(eigs)]
+        etas_fit = etas[np.abs(etas - etas[eigs == eigs.max()]) < .1 * etas[eigs == eigs.max()]]
+        eigs_fit = eigs[np.abs(etas - etas[eigs == eigs.max()]) < .1 * etas[eigs == eigs.max()]]
+        C = eigs_fit.max()
+        x0 = etas_fit[eigs_fit == C][0].value
+        if x0 == etas_fit[0].value:
+            A = (eigs_fit[-1] - C) / ((etas_fit[-1].value - x0)**2)
+        else:
+            A = (eigs_fit[0] - C) / ((etas_fit[0].value - x0)**2)
+        popt, pcov = curve_fit(chi_par,
+                                etas_fit.value,
+                                eigs_fit,
+                                p0=np.array([A, x0, C]))
+        eta_fit = popt[1]*u.us/u.mHz**2
+        eta_sig = np.sqrt((eigs_fit - chi_par(etas_fit.value, *popt)).std() / np.abs(popt[0]))*u.us/u.mHz**2
+    except:
+        popt=None
+        eta_fit=np.nan
+        eta_sig=np.nan
+    try:
+        if plot:
+            PlotFunc(dspec2,time2,freq2,SS,fd,tau,edges,eta_fit,eta_sig,etas,eigs,etas_fit,popt)
+            plt.savefig(name)
+            plt.close()
+    except:
+        print('Plotting Error',flush=True)
+    print('Chunk completed (eta = %s +- %s at %s)' %(eta_fit,eta_sig,freq2.mean()),flush=True)
+    return(eta_fit,eta_sig,freq2.mean(),time2.mean())
+
+def PlotFunc(dspec,time,freq,SS,fd,tau,
+            edges,eta_fit,eta_sig,etas,measure,etas_fit,fit_res,
+            tau_lim=None,method='eigenvalue'):
+    '''
+    Plotting script to look at invidivual chunks
+
+    Arguments
+    dspec -- 2D numpy array containing the dynamic spectrum
+    time -- 1D numpy array of the dynamic spectrum time bins (with units)
+    freq -- 1D numpy array of the dynamic spectrum frequency channels (with units)
+    SS -- 2D numpy array of the conjugate spectrum
+    fd -- 1D numpy array of the SS fd bins (with units)
+    tau -- 1D numpy array of the SS tau bins (with units)
+    edges -- 1D numpy array with the bin edges for theta-theta
+    eta_fit -- Best fit curvature
+    eta_sig -- Error on best fir curvature
+    etas -- 1D numpy array of curvatures searched over
+    measure -- 1D numpy array with largest eigenvalue (method = 'eigenvalue') or chisq value (method = 'chisq') for etas
+    etas_fit -- Subarray of etas used for fitting
+    fit_res -- Fit parameters for parabola at extremum
+    tau_lim -- Largest tau value for SS plots
+    method -- Either 'eigenvalue' or 'chisq' depending on how curvature was found
+    '''
+    fd_lim=min(2*edges.max(),fd.max().value)
+    if np.isnan(eta_fit):
+        eta=etas.mean()
+        thth_red, thth2_red, recov, model, edges_red,w,V = modeler(SS, tau, fd, etas.mean(), edges)
+    else:
+        eta=eta_fit
+        thth_red, thth2_red, recov, model, edges_red,w,V = modeler(SS, tau, fd, eta_fit, edges)
+    ththE_red=thth_red*0
+    ththE_red[ththE_red.shape[0]//2,:]=np.conjugate(V)*np.sqrt(w)
+    ##Map back to time/frequency space
+    recov_E=rev_map(ththE_red,tau,fd,eta,edges_red,isdspec = False)
+    model_E=np.fft.ifft2(np.fft.ifftshift(recov_E))[:dspec.shape[0],:dspec.shape[1]]
+    model_E*=(dspec.shape[0]*dspec.shape[1]/4)
+    model_E[dspec>0]=np.sqrt(dspec[dspec>0])*np.exp(1j*np.angle(model_E[dspec>0]))
+    model_E=np.pad(model_E,
+                    (   (0,SS.shape[0]-model_E.shape[0]),
+                        (0,SS.shape[1]-model_E.shape[1])),
+                    mode='constant',
+                    constant_values=0)
+    recov_E=np.abs(np.fft.fftshift(np.fft.fft2(model_E)))**2
+    model_E=model_E[:dspec.shape[0],:dspec.shape[1]]
+    N_E=recov_E[:recov_E.shape[0]//4,:].mean()
+
+    grid=plt.GridSpec(5,2)
+    plt.figure(figsize=(8,20))
+    plt.subplot(grid[0,0])
     plt.imshow(dspec,
-               aspect='auto',
-               origin='lower',
-               extent=dspec_ext,
-               vmin=0,
-               vmax=1)
-    plt.title('Dynamic Spectrum')
+            aspect='auto',
+            extent=ext_find(time.to(u.min),freq),
+            origin='lower',
+            vmin=0,vmax=dspec.max())
     plt.xlabel('Time (min)')
     plt.ylabel('Freq (MHz)')
-    plt.subplot(grid[0, 1])
-    plt.imshow(dspec2,
-               aspect='auto',
-               origin='lower',
-               extent=dspec_ext,
-               vmin=0,
-               vmax=1)
-    plt.title('Dynamic Spectrum Model')
+    plt.title('Data Dynamic Spectrum')
+    plt.subplot(grid[0,1])
+    plt.imshow(model[:dspec.shape[0],:dspec.shape[1]],
+            aspect='auto',
+            extent=ext_find(time.to(u.min),freq),
+            origin='lower',
+            vmin=0,vmax=dspec.max())
     plt.xlabel('Time (min)')
     plt.ylabel('Freq (MHz)')
-    plt.subplot(grid[1, 0])
+    plt.title('Model Dynamic Spectrum')
+    plt.subplot(grid[1,0])
     plt.imshow(np.abs(SS)**2,
-               norm=LogNorm(),
-               aspect='auto',
-               origin='lower',
-               extent=SS_ext,
-               vmin=SS_min,
-               vmax=SS_max)
-    plt.xlim((-fdm, fdm))
-    plt.ylim((0, taum))
-    plt.plot(fd, eta_low * (fd**2), 'k')
-    plt.plot(fd, eta_high * (fd**2), 'k')
-    plt.plot(fd, eta_fit * (fd**2), 'r')
-    plt.title('Secondary Spectrum')
+            norm=LogNorm(),
+            origin='lower',
+            aspect='auto',
+            extent=ext_find(fd,tau),
+            vmin=np.abs(SS).max()**2/100,vmax=np.abs(SS).max()**2)
+    plt.xlim((-fd_lim,fd_lim))
+    plt.ylim((0,tau_lim))
     plt.xlabel(r'$f_D$ (mHz)')
     plt.ylabel(r'$\tau$ (us)')
-    plt.subplot(grid[1, 1])
-    plt.imshow(np.abs(2 * SS2)**2,
-               norm=LogNorm(),
-               aspect='auto',
-               origin='lower',
-               extent=SS_ext,
-               vmin=SS_min,
-               vmax=SS_max)
-    plt.xlim((-fdm, fdm))
-    plt.ylim((0, taum))
-    plt.plot(fd, etas.min() * (fd**2), 'k')
-    plt.plot(fd, etas.max() * (fd**2), 'k')
-    plt.plot(fd, eta_fit * (fd**2), 'r')
-    plt.title('Secondary Spectrum Model')
+    plt.title('Data Secondary Spectrum')
+    plt.subplot(grid[1,1])
+    plt.imshow(np.abs(recov)**2,
+            norm=LogNorm(),
+            origin='lower',
+            aspect='auto',
+            extent=ext_find(fd,tau),
+            vmin=np.abs(SS).max()**2/100,vmax=np.abs(SS).max()**2)
+    plt.xlim((-fd_lim,fd_lim))
+    plt.ylim((0,tau_lim))
     plt.xlabel(r'$f_D$ (mHz)')
     plt.ylabel(r'$\tau$ (us)')
-
-    thth_min = np.median(np.abs(thth)) / 10
-    thth_max = np.max(np.abs(thth))
-
-    plt.subplot(grid[2, 0])
-    plt.imshow(np.abs(thth),
-               norm=LogNorm(),
-               aspect='auto',
-               origin='lower',
-               extent=[edges[0], edges[-1], edges[0], edges[-1]],
-               vmin=thth_min,
-               vmax=thth_max)
-    plt.title(r'$\theta-\theta$')
+    plt.title('Model Secondary Spectrum')
+    plt.subplot(grid[2,0])
+    plt.imshow(np.abs(thth_red)**2,
+            norm=LogNorm(),
+            origin='lower',
+            aspect='auto',
+            extent=[edges_red[0],edges_red[-1],edges_red[0],edges_red[-1]],
+            vmin=np.abs(thth_red).max()**2/100,vmax=np.abs(thth_red).max()**2)
     plt.xlabel(r'$\theta_1$')
     plt.ylabel(r'$\theta_2$')
-    plt.subplot(grid[2, 1])
-    plt.imshow(np.abs(thth2),
-               norm=LogNorm(),
-               aspect='auto',
-               origin='lower',
-               extent=[edges[0], edges[-1], edges[0], edges[-1]],
-               vmin=thth_min,
-               vmax=thth_max)
-    plt.title(r'$\theta-\theta$')
+    plt.title(r'Data $\theta-\theta$')
+    plt.subplot(grid[2,1])
+    plt.imshow(np.abs(thth2_red)**2,
+            norm=LogNorm(),
+            origin='lower',
+            aspect='auto',
+            extent=[edges_red[0],edges_red[-1],edges_red[0],edges_red[-1]],
+            vmin=np.abs(thth_red).max()**2/100,vmax=np.abs(thth_red).max()**2)
     plt.xlabel(r'$\theta_1$')
     plt.ylabel(r'$\theta_2$')
-    plt.subplot(grid[3, :])
-    plt.plot(etas, chisq)
-    if not err_str == None:
-        plt.axvline(eta_fit.value,label=r'%s $\pm$ %s $s^3$' % (fit_str, err_str))
-    elif eta_fit>0:
-        plt.axvline(eta_fit.value)
+    plt.title(r'Data $\theta-\theta$')
+    plt.subplot(grid[3,:])
+    plt.plot(etas,measure)
+    if not np.isnan(eta_fit):
+        exp_fit = int(('%.0e' % eta_fit.value)[2:])
+        exp_err = int(('%.0e' % eta_sig.value)[2:])
+        fmt = "{:.%se}" % (exp_fit - exp_err)
+        fit_string = fmt.format(eta_fit.value)[:2 + exp_fit - exp_err]
+        err_string = '0%s' % fmt.format(10**(exp_fit) + eta_sig.value)[1:]
+        
+        plt.plot(etas_fit,
+            chi_par(etas_fit.value, *fit_res),
+            label=r'$\eta$ = %s $\pm$ %s $s^3$' % (fit_string, err_string))
+        plt.legend()
+    if method == 'eigenvalue':
+        plt.title('Eigenvalue Search')
+        plt.ylabel(r'Largest Eigenvalue')
+    else:
+        plt.title('Chisquare Search')
+        plt.ylabel(r'$\chi^2$')
     plt.xlabel(r'$\eta$ ($s^3$)')
-    plt.ylabel(r'$\chi^2$')
-    plt.title('Curvature Search')
-    plt.legend()
+    plt.subplot(grid[4,0])
+    plt.imshow(np.angle(model_E),
+            cmap='twilight',
+            aspect='auto',
+            extent=ext_find(time.to(u.min),freq),
+            origin='lower',
+            vmin=-np.pi,vmax=np.pi)
+    plt.xlabel('Time (min)')
+    plt.ylabel('Freq (MHz)')
+    plt.title('Recovered Phases')
+    plt.subplot(grid[4,1])
+    plt.imshow(recov_E,
+            norm=LogNorm(),
+            origin='lower',
+            aspect='auto',
+            extent=ext_find(fd,tau),
+            vmin=N_E)
+    plt.xlim((-fd_lim,fd_lim))
+    plt.ylim((0,tau_lim))
+    plt.xlabel(r'$f_D$ (mHz)')
+    plt.ylabel(r'$\tau$ (us)')
+    plt.title('Recovered Wavefield')
+    plt.colorbar()
+    plt.tight_layout()
 
+def single_chunk_retrieval(params):
+    dspec2,edges,time2,freq2,eta,idx_t,idx_f = params
+    print("Starting Chunk %s-%s" %(idx_f,idx_t),flush=True)
+    npad = 3
+    fd = fft_axis(time2, u.mHz, npad)
+    tau = fft_axis(freq2, u.us, npad)
+
+    dspec_pad = np.pad(dspec2,
+                   ((0, npad * dspec2.shape[0]), (0, npad * dspec2.shape[1])),
+                   mode='constant',
+                   constant_values=dspec2.mean())
+
+    SS = np.fft.fft2(dspec_pad)
+    SS = np.fft.fftshift(SS)
+
+    try:
+        thth_red, thth2_red, recov, model, edges_red,w,V = modeler(SS, tau, fd, eta, edges)
+
+        ththE_red=thth_red*0
+        ththE_red[ththE_red.shape[0]//2,:]=np.conjugate(V)*np.sqrt(w)
+        ##Map back to time/frequency space
+        recov_E=rev_map(ththE_red,tau,fd,eta,edges_red,isdspec = False)
+        model_E=np.fft.ifft2(np.fft.ifftshift(recov_E))[:dspec2.shape[0],:dspec2.shape[1]]
+        model_E*=(dspec2.shape[0]*dspec2.shape[1]/4)
+        print("Chunk %s-%s success" %(idx_f,idx_t),flush=True)
+    except Exception as e:
+        print(e,flush=True)
+        model_E=np.zeros(dspec2.shape,dtype=complex)
+    return(model_E,idx_f,idx_t)
