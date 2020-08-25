b90e17245a8d466a159a4d9565abe04cd49288de
Daniel Reardon <dreardon@swin.edu.au>
2020-08-25 22:47:57 +1000

    only np slowft

--- scintools/scint_utils.py
+++ scintools/scint_utils.py
@@ -387,9 +387,6 @@ def slow_FT(dynspec, freqs):
     t*(f / fref), account for phase scaling of f_D.
     Given a uniform t axis, this reduces to a regular FT
 
-    Uses Olaf's c-implemation if possible, otherwise reverts
-    to a slow, pure Python / numpy method
-
     Reference freq is currently hardcoded to the middle of the band
 
     Parameters
@@ -401,38 +398,11 @@ def slow_FT(dynspec, freqs):
         Frequencies of the channels in dynspec
     """
 
-    from numpy.ctypeslib import ndpointer
-    import ctypes
-
-    dir = os.path.dirname(__file__)
-    filename = os.path.join(dir, 'fit_1d-response.so')
-    lib = ctypes.CDLL(filename)
-    # lib.omp_set_num_threads (4)   # if you do not want to use all cores
-
-    lib.comp_dft_for_secspec.argtypes = [
-        ctypes.c_int,   # ntime
-        ctypes.c_int,   # nfreq
-        ctypes.c_int,   # nr    Doppler axis
-        ctypes.c_double,  # r0
-        ctypes.c_double,  # delta r
-        ndpointer(dtype=np.float64,
-                  flags='CONTIGUOUS', ndim=1),  # freqs [nfreq]
-        ndpointer(dtype=np.float64,
-                  flags='CONTIGUOUS', ndim=1),  # src [ntime]
-        ndpointer(dtype=np.float64,
-                  flags='CONTIGUOUS', ndim=2),  # input pow [ntime,nfreq]
-        ndpointer(dtype=np.complex128,
-                  flags='CONTIGUOUS', ndim=2),  # result [nr,nfreq]
-    ]
-
     # cast dynspec as float 64
     dynspec = dynspec.astype(np.float64)
 
     ntime = dynspec.shape[0]
     nfreq = dynspec.shape[1]
-    r0 = np.fft.fftfreq(ntime)
-    delta_r = r0[1] - r0[0]
-    src = np.linspace(0, 1, ntime).astype('float64')
     src = np.arange(ntime).astype('float64')
 
     # declare the empty result array:
@@ -444,25 +414,14 @@ def slow_FT(dynspec, freqs):
     fscale = freqs / fref
     fscale = fscale.astype('float64')
 
-    # call the DFT:
-    if os.path.isfile(filename):
-        print("Computing slow FT using C-implementation, fit_1d-response")
-        lib.comp_dft_for_secspec(ntime, nfreq, ntime, min(r0), delta_r, fscale,
-                                 src, dynspec, SS)
+    ft = np.fft.fftfreq(ntime, 1)
 
-        # flip along time
-        SS = SS[::-1]
-    else:
-        print("C-implentation fit_1d-response not installed, "
-              "computing slowFT with numpy")
-        ft = np.fft.fftfreq(ntime, 1)
-
-        # Scaled array of t * f/fref
-        tscale = src[:, np.newaxis]*fscale[np.newaxis, :]
-        FTphase = -2j*np.pi*tscale[:, np.newaxis, :] * \
-            ft[np.newaxis, :, np.newaxis]
-        SS = np.sum(dynspec[:, np.newaxis, :]*np.exp(FTphase), axis=0)
-        SS = np.fft.fftshift(SS, axis=0)
+    # Scaled array of t * f/fref
+    tscale = src[:, np.newaxis]*fscale[np.newaxis, :]
+    FTphase = -2j*np.pi*tscale[:, np.newaxis, :] * \
+        ft[np.newaxis, :, np.newaxis]
+    SS = np.sum(dynspec[:, np.newaxis, :]*np.exp(FTphase), axis=0)
+    SS = np.fft.fftshift(SS, axis=0)
 
     # Still need to FFT y axis, should change to pyfftw for memory and
     #   speed improvement
